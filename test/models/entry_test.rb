5ebe2f30803b23ad32feafb1d08b4dee4768a16c
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-11-01 20:49:32 -0400

    add simple test

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test "is valid with valid at attibutes" do
+
+  entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 10, fats: 0, calories: 1000)
+
+  assert entry.save
+end
 end
